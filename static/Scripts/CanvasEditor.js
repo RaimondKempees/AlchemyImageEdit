83b0b4aa0286f8d20204f3a5b1c5ce57a9dad8d2
Frank Taylor <frank.m.taylor08@gmail.com>
2015-06-16 14:48:58 +0100

    proper color conversion

--- static/Scripts/CanvasEditor.js
+++ static/Scripts/CanvasEditor.js
@@ -13,13 +13,13 @@ var editorUi = {
         }
     },
     bindEvents: function () {
+        var _this = this;
         var colorInputs = document.querySelectorAll('.js-editorColor');
         console.log(colorInputs);
         [].forEach.call(colorInputs, function (colorInput) {
             colorInput.addEventListener('change', function (e) {
-                var colorObj = this.classes.colorObj();
-                console.log(this.value);
-
+                var colorObj = _this.classes.ColorObj();
+                console.log(colorObj)
                 editWithCanvas.editImageOnCanvas(colorObj);
             });
         });
@@ -47,25 +47,20 @@ editWithCanvas =  {
             this.context = this.canvasEl.getContext('2d');
             this.canvasEl.width = this.imageEl.offsetWidth;
             this.canvasEl.height = this.imageEl.offsetHeight;
-            this.canvasEl.style.zIndex = 100;
-            this.canvasEl.style.position = 'fixed';
-            this.canvasEl.style.left = this.imageEl.offsetLeft;
-            this.canvasEl.style.top = this.imageEl.offsetTop;
-            document.querySelector('body').appendChild(this.canvasEl);
+            this.imageEl.parentNode.appendChild(this.canvasEl);
         },
         addImageToCanvas: function (imageEl) {
             this.context.drawImage(imageEl, 0, 0, this.canvasEl.offsetWidth, this.canvasEl.offsetHeight);
         },
-
         editImageOnCanvas: function ( colorObj) {
             this.context.clearRect(0,0,this.canvasEl.offsetHeight, this.canvasEl.offsetWidth)
             this.addImageToCanvas(this.imageEl);
             var pixels = this.context.getImageData(0, 0, this.canvasEl.width, this.canvasEl.height),i = 0,brightness;
             for (; i < pixels.data.length; i += 4) {
                     brightness = ((3 * pixels.data[i] + 4 * pixels.data[i + 1] + pixels.data[i + 2]) >>> 3) / 256;
-                    pixels.data[i] = ((colorObj.r * brightness) + 0.1) >> 0;
-                    pixels.data[i + 1] = ((colorObj.g * brightness) + 0.1) >> 0
-                    pixels.data[i + 2] = ((colorObj.b * brightness) + 0.1) >> 0
+                    pixels.data[i] = ((colorObj.red * brightness) + 0.1) >> 0;
+                    pixels.data[i + 1] = ((colorObj.green * brightness) + 0.1) >> 0;
+                    pixels.data[i + 2] = ((colorObj.blue * brightness) + 0.1) >> 0;
                 }
               this.context.putImageData(pixels, 0, 0);
         },
