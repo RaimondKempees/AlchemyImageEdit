0f7dd84fe5e86d0ff205c8b23591638048132395
Raimond Kempees <raimond.kempees@tomtom.com>
2015-06-16 10:41:53 +0100

    Initial commit

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,20 @@
+using Alchemy4Tridion.Plugins;
+using System.Web.Http;
+
+namespace ImageEdit.Controllers
+{
+    [AlchemyRoutePrefix(typeof(AlchemyPlugin), "Service")]
+    public class ServiceController : AlchemyApiController
+    {
+        // GET /Alchemy/Plugins/YourPluginName/api/Service/Hello
+        [HttpGet]
+        [Route("Hello")]
+        public string Hello()
+        {
+            return "Hello World";
+        }
+
+
+
+    }
+}
---
+++ packages/Alchemy4Tridion.Plugin.1.0.0-alpha4/content/Controllers/ServiceController.cs.pp
@@ -0,0 +1,17 @@
+﻿using Alchemy4Tridion.Plugins;
+using System.Web.Http;
+
+namespace $rootnamespace$.Controllers
+{
+    [AlchemyRoutePrefix(typeof(AlchemyPlugin), "Service")]
+    public class ServiceController : AlchemyApiController
+    {
+        // GET /Alchemy/Plugins/YourPluginName/api/Service/Hello
+        [HttpGet]
+        [Route("Hello")]
+        public string Hello()
+        {
+            return "Hello World";
+        }
+    }
+}
