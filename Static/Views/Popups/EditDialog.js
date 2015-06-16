0495ce96810b2df37dae72a4df18fa08b46ccb8e
Raimond Kempees <kempees@radagio.com>
2015-06-16 16:21:25 +0100

    OMG it's working!

--- Static/Views/Popups/EditDialog.js
+++ Static/Views/Popups/EditDialog.js
@@ -33,9 +33,9 @@ Alchemy.Popups.EditDialog.prototype.initialize = function EditDialog$initialize(
 		c.colorBlue = $("#ColorBlue");
 		c.colorGreen = $("#ColorGreen");
 
-		$evt.addEventHandler(c.colorRed, "change", this.getDelegate(this._generatePreview));
-		$evt.addEventHandler(c.colorBlue, "change", this.getDelegate(this._generatePreview));
-		$evt.addEventHandler(c.colorGreen, "change", this.getDelegate(this._generatePreview));
+		$evt.addEventHandler(c.colorRed, "change", this.getDelegate(this._updateGeneratedPreview));
+		$evt.addEventHandler(c.colorBlue, "change", this.getDelegate(this._updateGeneratedPreview));
+		$evt.addEventHandler(c.colorGreen, "change", this.getDelegate(this._updateGeneratedPreview));
 
 		c.previewArea = $("#PreviewArea");
 
@@ -117,6 +117,54 @@ Alchemy.Popups.EditDialog.prototype.onThumbnailLoadFailed = function Component$o
 
 };
 
+Alchemy.Popups.EditDialog.editWithCanvas = {
+	init: function (imageEl) {
+		this.imageEl = imageEl;
+		this.createCanvas();
+		this.hideImage();
+		this.addImageToCanvas(imageEl);
+	},
+	hideImage: function (imageEl) {
+		this.imageEl.style.display = 'none';
+	},
+	setupCanvas: function () {
+		this.createCanvas();
+		this.hideImage();
+	},
+	createCanvas: function () {
+		this.canvasEl = document.createElement('canvas');
+		this.context = this.canvasEl.getContext('2d');
+		this.canvasEl.width = this.imageEl.offsetWidth;
+		this.canvasEl.height = this.imageEl.offsetHeight;
+		this.imageEl.parentNode.appendChild(this.canvasEl);
+	},
+	addImageToCanvas: function (imageEl) {
+		this.context.drawImage(imageEl, 0, 0, this.canvasEl.offsetWidth, this.canvasEl.offsetHeight);
+	},
+	editImageOnCanvas: function (colorObj, userBrightness) {
+		this.context.clearRect(0, 0, this.canvasEl.offsetHeight, this.canvasEl.offsetWidth);
+		this.addImageToCanvas(this.imageEl);
+		console.log(colorObj);
+		var pixels = this.context.getImageData(0, 0, this.canvasEl.width, this.canvasEl.height), i = 0, brightness;
+		for (; i < pixels.data.length; i += 4) {
+			brightness = ((3 * pixels.data[i] + 4 * pixels.data[i + 1] + pixels.data[i + 2]) >>> 3) / 256;
+			brightness = userBrightness !== undefined ? ((brightness * userBrightness) + brightness) : brightness;
+
+
+			pixels.data[i] = colorObj.red != 0 ? ((colorObj.red * brightness) + 0.1) >> 0 : ((pixels.data[i] * brightness) + .1);
+			pixels.data[i + 1] = colorObj.green != 0 ? ((colorObj.green * brightness) + 0.1) >> 0 : ((pixels.data[i + 1] * brightness) + .1);
+			pixels.data[i + 2] = colorObj.blue != 0 ? ((colorObj.blue * brightness) + 0.1) >> 0 : ((pixels.data[i + 2] * brightness) + .1);
+		}
+		this.context.putImageData(pixels, 0, 0);
+	},
+	setImageAttributes: function (rgbObj, brightness) {
+
+	},
+	getImage: function () {
+		return this.context.toDataURL();
+	}
+};
+
 Alchemy.Popups.EditDialog.prototype.onThumbnailLoad = function Component$onThumbnailLoad()
 {
 	var p = this.properties;
@@ -129,7 +177,13 @@ Alchemy.Popups.EditDialog.prototype.onThumbnailLoad = function Component$onThumb
 		var multimediaImage = p.controls.MultimediaImage;
 		multimediaImage.src = p.thumbnailImage.src;
 		p.thumbnailImage = null;
+
+		Alchemy.Popups.EditDialog.editWithCanvas.init(p.controls.MultimediaImage);
 	}
+};
+
+Alchemy.Popups.EditDialog.prototype._updateGeneratedPreview = function EditDialog$_generatePreview(e) {
+	Alchemy.Popups.EditDialog.editWithCanvas.editImageOnCanvas(this.getInstructions());
 };
 
 Alchemy.Popups.EditDialog.prototype._generatePreview = function EditDialog$_generatePreview(e)
