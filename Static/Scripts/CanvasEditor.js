b4585dfc3d7b2ff6bcc2f334ed5dac51f206f042
Frank Taylor <frank.m.taylor08@gmail.com>
2015-06-18 18:48:34 -0400

    Added the UI part of croping

--- Static/Scripts/CanvasEditor.js
+++ Static/Scripts/CanvasEditor.js
@@ -4,7 +4,7 @@ var editorUi = {
     },
     classes: {
         ColorObj: function () {
-            var colorInputs = document.querySelectorAll('.js-editorColor'),
+            var colorInputs = document.querySelectorAll('[id*="Color"]'),
                 colorObj = {};
             [].forEach.call(colorInputs, function (colorInput) {
                 colorObj[colorInput.name] = colorInput.value;
@@ -14,18 +14,26 @@ var editorUi = {
     },
     bindEvents: function () {
         var _this = this,
-            colorInputs = document.querySelectorAll('.js-editorColor'),
-            brightnessInput = document.querySelector('.js-editorColorBrightness');
+            colorInputs = document.querySelectorAll('[id*="Color"]'),
+            brightnessInput = document.querySelector('#ImageBrightness'),
+            cropImageInput = document.querySelector('#CropImage'),
+            saveCropButton = document.querySelector('#SaveCrop');
         [].forEach.call(colorInputs, function (colorInput) {
             colorInput.addEventListener('change', function (e) {
                 var colorObj = _this.classes.ColorObj();
-                editWithCanvas.editImageOnCanvas(colorObj);
+                editWithCanvas.drawCanvas(colorObj);
             });
         });
         brightnessInput.addEventListener('change', function (e) {
                 var colorObj = _this.classes.ColorObj(),
                     brightness = (this.value /100);
-                editWithCanvas.editImageOnCanvas(colorObj, brightness);
+                editWithCanvas.drawCanvas(colorObj, brightness);
+        });
+        cropImageInput.addEventListener('change', function (e) {
+            editWithCanvas.cropData.allowCropping = e.target.checked;
+        });
+        saveCropButton.addEventListener('click', function (e) {
+            editWithCanvas.cropCanvas();
         });
     }
 };
@@ -38,6 +46,41 @@ editWithCanvas =  {
             this.createCanvas();
             this.hideImage();
             this.addImageToCanvas(imageEl);
+            this.bindEvents();
+        },
+        cropData: {
+            allowCropping: false,
+            isCropping: false,
+            hasCropping: false,
+            x: 0,
+            y: 0,
+            w: 1,
+            h: 1
+        },
+        bindEvents: function () {
+            this.canvasEl.addEventListener('mousedown', function (e) {
+                if (editWithCanvas.cropData.allowCropping) {
+                    editWithCanvas.cropData.isCropping = true;
+                    editWithCanvas.cropData.x = e.offsetX;
+                    editWithCanvas.cropData.y = e.offsetY;
+                }
+            });
+            this.canvasEl.addEventListener('mousemove', function (e) {
+                if(editWithCanvas.cropData.isCropping) {
+                    editWithCanvas.cropData.w = editWithCanvas.cropData.x + e.offsetX;
+                    editWithCanvas.cropData.h = editWithCanvas.cropData.y + e.offsetY;
+                }
+            });
+            this.canvasEl.addEventListener('mouseup', function (e) {
+                if (editWithCanvas.cropData.allowCropping) {
+                    editWithCanvas.cropData.isCropping = false;
+                    editWithCanvas.cropData.hasCropping = true;
+                    editWithCanvas.cropData.w = editWithCanvas.cropData.x + e.offsetX;
+                    editWithCanvas.cropData.h = editWithCanvas.cropData.y + e.offsetY;
+                    console.log(editWithCanvas.cropData);
+                }
+
+            })
         },
         hideImage: function (imageEl) {
             this.imageEl.style.display = 'none';
@@ -56,10 +99,7 @@ editWithCanvas =  {
         addImageToCanvas: function (imageEl) {
             this.context.drawImage(imageEl, 0, 0, this.canvasEl.offsetWidth, this.canvasEl.offsetHeight);
         },
-        editImageOnCanvas: function ( colorObj,userBrightness) {
-            this.context.clearRect(0,0,this.canvasEl.offsetHeight, this.canvasEl.offsetWidth);
-            this.addImageToCanvas(this.imageEl);
-            console.log(colorObj);
+        getCanvasPixels: function (colorObj, userBrightness) {
             var pixels = this.context.getImageData(0, 0, this.canvasEl.width, this.canvasEl.height),i = 0,brightness;
             for (; i < pixels.data.length; i += 4) {
                     brightness = ((3 * pixels.data[i] + 4 * pixels.data[i + 1] + pixels.data[i + 2]) >>> 3) / 256;
@@ -70,7 +110,33 @@ editWithCanvas =  {
                     pixels.data[i + 1] = colorObj.green != 0 ?  ((colorObj.green * brightness) + 0.1) >> 0 : ((pixels.data[i+1] * brightness) + .1) ;
                     pixels.data[i + 2] = colorObj.blue != 0 ? ((colorObj.blue * brightness) + 0.1) >> 0 : ((pixels.data[i+2] * brightness) + .1) ;
                 }
-              this.context.putImageData(pixels, 0, 0);
+             return pixels;
+        },
+        showCroppingFrame: function() {
+
+            if (this.cropData.isCropping || this.cropData.hasCropping) {
+                this.context.lineWidth = 4;
+                this.context.strokeStyle = "#444444";
+                this.context.fillStyle = 'rgba(200,200,200, .5)';
+                this.context.fillRect(this.cropData.x, this.cropData.y, this.cropData.w, this.cropData.h);
+            }
+        },
+        cropCanvas: function () {
+            //this.canvasEl.width = this.cropData.w;
+            //this.canvasEl.height = this.cropData.h;
+            this.context.translate(this.cropData.w, this.cropData.h);
+        },
+        drawCanvas: function ( colorObj,userBrightness) {
+            var _this = this;
+            _this.context.clearRect(0,0,this.canvasEl.offsetHeight, this.canvasEl.offsetWidth);
+            _this.addImageToCanvas(this.imageEl);
+            var pixels = _this.getCanvasPixels(colorObj, userBrightness);
+            
+            this.context.putImageData(pixels, 0, 0);
+            _this.showCroppingFrame();
+             window.requestAnimationFrame(function() {
+                _this.drawCanvas(colorObj, userBrightness);
+             })
         },
         setImageAttributes: function(rgbObj, brightness) {
 
